74a1f95fc3b8fc3a2145cb8772f23ddf47cba0ed
Jake Soenneker <jake@soenneker.com>
2026-01-05 13:31:27 -0600

    Reduces closure allocation

--- src/RedisClient.cs
+++ src/RedisClient.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -14,43 +13,43 @@ namespace Soenneker.Redis.Client;
 /// <inheritdoc cref="IRedisClient"/>
 public sealed class RedisClient : IRedisClient
 {
-    private readonly AsyncSingleton<ConnectionMultiplexer> _client;
-
     private readonly ILogger<RedisClient> _logger;
+    private readonly string _connectionString;
+
+    private readonly AsyncSingleton<ConnectionMultiplexer> _client;
 
     public RedisClient(IConfiguration config, ILogger<RedisClient> logger)
     {
         _logger = logger;
+        _connectionString = config.GetValueStrict<string>("Azure:Redis:ConnectionString"); // TODO: not reliant on Azure namespace
 
-        _client = new AsyncSingleton<ConnectionMultiplexer>(async () =>
-        {
-            var connectionString = config.GetValueStrict<string>("Azure:Redis:ConnectionString"); // TODO: not reliant on Azure namespace
-
-            _logger.LogDebug(">> REDIS: Connecting to {endpoint} ...", connectionString);
-
-            ConfigurationOptions options = ConfigurationOptions.Parse(connectionString);
-            options.AllowAdmin = true;
-
-            return await ConnectionMultiplexer.ConnectAsync(options).NoSync();
-        });
+        // No closure: method group
+        _client = new AsyncSingleton<ConnectionMultiplexer>(ConnectAsync);
     }
 
-    public ValueTask<ConnectionMultiplexer> Get(CancellationToken cancellationToken = default)
+    private async ValueTask<ConnectionMultiplexer> ConnectAsync()
     {
-        return _client.Get(cancellationToken);
+        _logger.LogDebug(">> REDIS: Connecting to {endpoint} ...", _connectionString);
+
+        ConfigurationOptions options = ConfigurationOptions.Parse(_connectionString);
+        options.AllowAdmin = true;
+
+        return await ConnectionMultiplexer.ConnectAsync(options)
+                                          .NoSync();
     }
+
+    public ValueTask<ConnectionMultiplexer> Get(CancellationToken cancellationToken = default) =>
+        _client.Get(cancellationToken);
 
     public ValueTask DisposeAsync()
     {
         _logger.LogDebug(">> REDIS: Disposing...");
-
         return _client.DisposeAsync();
     }
 
     public void Dispose()
     {
         _logger.LogDebug(">> REDIS: Disposing...");
-
         _client.Dispose();
     }
 }
